69d70d7819a7fdc8389c8fbc01b21bff545f7ed3
shohag0007 <merinedrive007@gmail.com>
2024-03-23 00:05:08 +0600

    on going career hub project

--- src/components/Job/Job.jsx
+++ src/components/Job/Job.jsx
@@ -1,4 +1,5 @@
-
+import { MdAddLocationAlt } from "react-icons/md";
+import { CiDollar } from "react-icons/ci";
 
 const Job = ({job}) => {
    const {logo, job_title, company_name, remote_or_onsite, location, job_type, salary} = job;
@@ -13,6 +14,11 @@ const Job = ({job}) => {
     <div>
       <button className="px-5 py-2 font-extrabold border rounded border-[#7E90FE] mr-4 text-[#7E90FE]">{ remote_or_onsite}</button>
     <button className="px-5 py-2 font-extrabold border rounded border-[#7E90FE] mr-4 text-[#7E90FE]" >{job_type}</button>
+    </div>
+
+    <div className="mt-4 flex">
+      <h2 className="flex mr-4"><MdAddLocationAlt className="text-2xl mr-2"></MdAddLocationAlt>{location}</h2>
+     <h2 className="flex"><CiDollar className="text-2xl "></CiDollar>{salary}</h2>
     </div>
     <div className="card-actions ">
       <button className="btn btn-primary">View Details</button>
