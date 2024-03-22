e7dea4e6bc6989187b9b605c69fdfb1e27f4b22c
shohag0007 <merinedrive007@gmail.com>
2024-03-22 22:58:47 +0600

    career jobs project

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,11 @@
+
+
+const Banner = () => {
+    return (
+        <div className="text-center text-5xl">
+            <h2>Home Page Banner</h2>
+        </div>
+    );
+};
+
+export default Banner;
